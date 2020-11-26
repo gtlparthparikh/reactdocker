1fd34f32d482df8baa5437dafae785bc90d3c618
parth parikh <parth.parikh@thegatewaycorp.com>
2020-11-26 09:37:57 +0530

    sample

---
+++ src/De.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class De extends Component {
+    render() {
+        return (
+            <div>
+                And another Component.
+            </div>
+        )
+    }
+}
+
+export default De
